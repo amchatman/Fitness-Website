9dbef4812e576f7c91c946456df021d5ec967852
Ashley Chatman <49164365+amchatman@users.noreply.github.com>
2022-05-22 20:57:18 -0400

    Update Navigation Bar
    Navigation Bar
    Update Navigation bar to make more responsive
    Added a mobile menu section
    
    Other
    Delete other content to make the website more responsive

---
+++ main.js
@@ -0,0 +1,7 @@
+/*eslint-env es6*//*global require*/
+let mobile = document.querySelector('.mobile-nav');
+    
+mobile.addEventListener('click', function(){
+    this.classList.toggle('is-active');
+                            
+ });
